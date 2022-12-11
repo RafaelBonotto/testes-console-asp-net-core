62e242f104784a8fff4f9b002e2f33d6d1a9ba44
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-11 14:44:19 -0300

    Projeto Console

---
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+static Object Sub(Object a, Object b)
+{
+    string x = a.ToString();
+    string y = b.ToString();
+
+    foreach (var caracter in y)
+        foreach (var caract in x)
+            if (caract.Equals(caracter))
+                x.Remove(caract);
+
+   return x;
+}
+
+Console.WriteLine(Sub("Yes or no", "orn "));

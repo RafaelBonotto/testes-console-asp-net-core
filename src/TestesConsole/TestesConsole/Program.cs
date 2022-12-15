b67ec8b2f4462da73828dc17864a9cf4360be067
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-14 23:19:52 -0300

    checkin

--- src/TestesConsole/TestesConsole/Program.cs
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -2,15 +2,32 @@
 
 static Object Sub(Object a, Object b)
 {
-    string x = a.ToString();
-    string y = b.ToString();
+    string A = a.ToString();
+    string B = b.ToString();
+    string response = string.Empty;
+    foreach (var caracter in B)
+    {
+        while (A.Contains(caracter))
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i].Equals(caracter))
+                {
+                    A = A.Substring(0, (i - 1)) + A.Substring(i);
+                    break;
+                }
+            }
+        }
+    }        
 
-    foreach (var caracter in y)
-        foreach (var caract in x)
-            if (caract.Equals(caracter))
-                x.Remove(caract);
-
-   return x;
+   return A;
 }
 
 Console.WriteLine(Sub("Yes or no", "orn "));
+
+var str = "12345";
+
+
+//string str1 = str.Substring(0, (3 - 1));
+//var str2 = str.Substring(3);
+//Console.WriteLine(str1 + str2);

91f2c86ff12f4d2111f972ece20fbbc7372e7c36
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-16 23:57:34 -0300

    Invertendo uma string

--- src/TestesConsole/TestesConsole/Program.cs
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -1,33 +1,11 @@
-﻿using System.Linq;
+﻿
+using System.Text;
 
-static Object Sub(Object a, Object b)
-{
-    string A = a.ToString();
-    string B = b.ToString();
-    string response = string.Empty;
-    foreach (var caracter in B)
-    {
-        while (A.Contains(caracter))
-        {
-            for (int i = 0; i < A.Length; i++)
-            {
-                if (A[i].Equals(caracter))
-                {
-                    A = A.Substring(0, (i - 1)) + A.Substring(i);
-                    break;
-                }
-            }
-        }
-    }        
+var input = "dlrow";
+StringBuilder response = new();
+var length = input.Length - 1;
 
-   return A;
-}
+for (int i = length; i >= 0 ; i--)
+    response.Append(input[i].ToString());
 
-Console.WriteLine(Sub("Yes or no", "orn "));
-
-var str = "12345";
-
-
-//string str1 = str.Substring(0, (3 - 1));
-//var str2 = str.Substring(3);
-//Console.WriteLine(str1 + str2);
+Console.WriteLine(response.ToString());

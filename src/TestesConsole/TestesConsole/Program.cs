79bb63b56a91f37a282472354de7a9fca0358e4c
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-16 23:59:21 -0300

    Invertendo string 2

--- src/TestesConsole/TestesConsole/Program.cs
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -2,10 +2,12 @@
 using System.Text;
 
 var input = "dlrow";
-StringBuilder response = new();
-var length = input.Length - 1;
+//StringBuilder response = new();
+//var length = input.Length - 1;
 
-for (int i = length; i >= 0 ; i--)
-    response.Append(input[i].ToString());
+//for (int i = length; i >= 0 ; i--)
+//    response.Append(input[i].ToString());
 
-Console.WriteLine(response.ToString());
+//Console.WriteLine(response.ToString());
+
+Console.WriteLine(new string(input.Reverse().ToArray()));

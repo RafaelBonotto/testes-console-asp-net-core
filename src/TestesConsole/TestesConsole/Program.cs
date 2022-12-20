7d44baf6facf4f356c7f8b686551e1a6ec119cc6
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-20 09:53:28 -0300

    Code Wars Challenge Count by x (Arrays C#)

--- src/TestesConsole/TestesConsole/Program.cs
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -3,7 +3,7 @@
 static int[] CountBy(int x, int n)
 {
     List<int> response = new();
-    int incremento = x;
+    int incremento = 1;
 
     while (incremento <= n)
     {
@@ -18,3 +18,22 @@ foreach (var item in resp)
 {
     Console.Write(item + " , ");
 }
+
+/* Code Wars Challenge Count by x (Arrays C#)
+
+[TestFixture]
+public static class Test
+{
+  [Test]
+  public static void CountBy1()
+  {
+    Assert.AreEqual(new int[] {1,2,3,4,5}, Kata.CountBy(1,5), "Array does not match");
+  }
+  
+  [Test]
+  public static void CountBy2()
+  {
+    Assert.AreEqual(new int[] {2,4,6,8,10}, Kata.CountBy(2,5), "Array does not match");
+  }
+}
+ */

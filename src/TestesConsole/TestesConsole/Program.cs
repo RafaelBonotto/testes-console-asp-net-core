023fb5ef917ec192bbe5c1379704333630df9bce
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-21 14:20:43 -0300

    teste codewars ArrayDiff

--- src/TestesConsole/TestesConsole/Program.cs
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -1,39 +1,55 @@
-﻿using System.Collections.Generic;
+﻿int[] ints1 = { 5, 3, 9, 7, 5, 9, 3, 7 };
+int[] ints2 = { 8, 3, 6, 4, 4, 9, 1, 0 };
 
-static int[] CountBy(int x, int n)
+IEnumerable<int> union = ints1.Intersect(ints2);
+
+foreach (int num in union)
 {
-    List<int> response = new();
-    int incremento = 1;
+    Console.Write("{0} ", num);
+}
 
-    while (incremento <= n)
+
+
+
+
+
+//static int[] ArrayDiff(int[] a, int[] b)
+//{
+//    List<int> response = new();
+
+//    if(! a.Any())
+//        return response.ToArray();
+
+//    if (!b.Any())
+//        return a;
+
+//    foreach (var item in a.ToList())
+//        foreach (var rem in b.ToList())
+//            if (item != rem)
+//                response.Add(item);
+
+//    return response.ToArray();
+//}
+
+/*
+ namespace Solution 
+{
+  using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+  public class SolutionTest
+  {
+    [Test]
+    public void SampleTest()
     {
-        response.Add(x * incremento);
-        incremento++;
+      Assert.AreEqual(new int[] {2},       Kata.ArrayDiff(new int[] {1, 2},    new int[] {1}));
+      Assert.AreEqual(new int[] {2, 2},    Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {1}));
+      Assert.AreEqual(new int[] {1},       Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {2}));
+      Assert.AreEqual(new int[] {1, 2, 2}, Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {}));
+      Assert.AreEqual(new int[] {},        Kata.ArrayDiff(new int[] {},        new int[] {1, 2}));
     }
-    return response.ToArray();
-}
-
-var resp = CountBy(2, 5);
-foreach (var item in resp)
-{
-    Console.Write(item + " , ");
-}
-
-/* Code Wars Challenge Count by x (Arrays C#)
-
-[TestFixture]
-public static class Test
-{
-  [Test]
-  public static void CountBy1()
-  {
-    Assert.AreEqual(new int[] {1,2,3,4,5}, Kata.CountBy(1,5), "Array does not match");
-  }
-  
-  [Test]
-  public static void CountBy2()
-  {
-    Assert.AreEqual(new int[] {2,4,6,8,10}, Kata.CountBy(2,5), "Array does not match");
   }
 }
+ 
  */

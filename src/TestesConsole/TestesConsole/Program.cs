449fa65129839cee99187664629eaf19e041678c
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-03 09:33:25 -0300

    code wars maskfy

--- src/TestesConsole/TestesConsole/Program.cs
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -1,55 +1,15 @@
-﻿int[] ints1 = { 5, 3, 9, 7, 5, 9, 3, 7 };
-int[] ints2 = { 8, 3, 6, 4, 4, 9, 1, 0 };
-
-IEnumerable<int> union = ints1.Intersect(ints2);
-
-foreach (int num in union)
+﻿static string Maskify(string cc)
 {
-    Console.Write("{0} ", num);
+    string mask = string.Empty;
+
+
+
+
+    return mask;
 }
-
-
-
-
-
-
-//static int[] ArrayDiff(int[] a, int[] b)
-//{
-//    List<int> response = new();
-
-//    if(! a.Any())
-//        return response.ToArray();
-
-//    if (!b.Any())
-//        return a;
-
-//    foreach (var item in a.ToList())
-//        foreach (var rem in b.ToList())
-//            if (item != rem)
-//                response.Add(item);
-
-//    return response.ToArray();
-//}
 
 /*
- namespace Solution 
-{
-  using NUnit.Framework;
-  using System;
-
-  [TestFixture]
-  public class SolutionTest
-  {
-    [Test]
-    public void SampleTest()
-    {
-      Assert.AreEqual(new int[] {2},       Kata.ArrayDiff(new int[] {1, 2},    new int[] {1}));
-      Assert.AreEqual(new int[] {2, 2},    Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {1}));
-      Assert.AreEqual(new int[] {1},       Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {2}));
-      Assert.AreEqual(new int[] {1, 2, 2}, Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {}));
-      Assert.AreEqual(new int[] {},        Kata.ArrayDiff(new int[] {},        new int[] {1, 2}));
-    }
-  }
-}
- 
+    Assert.AreEqual("############5616", Kata.Maskify("4556364607935616"));
+    Assert.AreEqual("1", Kata.Maskify("1"));
+    Assert.AreEqual("#1111", Kata.Maskify("11111")); 
  */

58bac62e5c3eb16364c3cd48364c24dc510a992b
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-03 09:54:16 -0300

    CODE WARS 'Credit Card Mask'

--- src/TestesConsole/TestesConsole/Program.cs
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -1,15 +1,26 @@
-﻿static string Maskify(string cc)
-{
-    string mask = string.Empty;
-
-
-
-
-    return mask;
-}
-
-/*
+﻿
+// CODE WARS 'Credit Card Mask'
+/* 
     Assert.AreEqual("############5616", Kata.Maskify("4556364607935616"));
     Assert.AreEqual("1", Kata.Maskify("1"));
     Assert.AreEqual("#1111", Kata.Maskify("11111")); 
  */
+var mask = "11111";
+Console.WriteLine(Maskify(mask));
+
+static string Maskify(string cc)
+{
+    if (cc.Length < 4)
+        return cc;
+
+    var aux = cc.Substring(cc.Length - 4);
+    string ret = string.Empty;
+
+    for (int i = 0; i < cc.Length - 4; i++)
+        ret += "#";
+
+    ret += aux;
+
+    return ret;
+}
+

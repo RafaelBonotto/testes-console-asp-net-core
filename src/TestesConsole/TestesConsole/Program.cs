c9fd71f6574e3cdeb61955b5af98fa03ccbe6600
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-04 13:42:50 -0300

    CODE WARS - 'Convert a String to a Number!'

--- src/TestesConsole/TestesConsole/Program.cs
+++ src/TestesConsole/TestesConsole/Program.cs
@@ -1,20 +1,7 @@
-﻿
-// CODE WARS 'Credit Card Mask'
-/* 
-    Assert.AreEqual("############5616", Kata.Maskify("4556364607935616"));
-    Assert.AreEqual("1", Kata.Maskify("1"));
-    Assert.AreEqual("#1111", Kata.Maskify("11111")); 
- */
-var mask = "11111";
-Console.WriteLine(Maskify(mask));
+﻿// CODE WARS - 'Convert a String to a Number!'
+//Assert.AreEqual(-7, Kata.StringToNumber("-7"));
 
-static string Maskify(string cc)
-{
-    int len = cc.Length;
 
-    if (len <= 4)
-        return cc;
-
-    return cc.Substring(len - 4).PadLeft(len, '#');
-}
-
+Console.WriteLine(Func("-7"));
+static int Func(string number)
+    => Convert.ToInt32(number);
